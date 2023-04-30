395c5b2f068475561b8779af96b0272680d17a4d
Carlos Sandoval <csandovalc@unal.edu.co>
2023-04-29 20:08:31 -0500

    feat(trigonometry): pythagoras theorem implemented

---
+++ src/Trigonometry.java
@@ -0,0 +1,7 @@
+import java.lang.*;
+
+public class Trigonometry {
+    public static float pythagorasTheorem(float a, float b) {
+        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
+}
