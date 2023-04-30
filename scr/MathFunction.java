3348113382fdc1ba6e6a54ed1b2ecf50d8ecf382
glondonot <glondonot@unal.edu.co>
2023-04-30 10:01:13 -0500

    commit

--- scr/MathFunction.java
+++
@@ -1,5 +0,0 @@
-public class MathFunction {
-    public static void main(String[] args) {
-        system.out.println(7+6)
-    }
-}
--- src/MathFuctions.java
+++
@@ -1,5 +0,0 @@
-public class MathFuctions {
-    public static int divideNumber(int numberA, int numberB){
-        return numberA / numberB;
-    }
-}
